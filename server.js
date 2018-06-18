36e165a7ca341fff1ddc1733b1aa984949ea3b58
Daniel Moser <danielmoser49@gmail.com>
2018-06-18 15:39:26 -0500

    add path to server.js for production deployment

--- server.js
+++ server.js
@@ -10,5 +10,18 @@ mongoose.connect(keys.mongoURI)
 
 require('./routes/board')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js/main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html if
+  // doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
